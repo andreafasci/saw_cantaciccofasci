c9b6cab352afda8c3791b7ebb2cd74cc7adfbd5d
andrea <andreafasci@gmail.com>
2017-05-09 13:23:56 +0100

    Fixed AutoLogin bug

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	setcookie('login', null, time()-100, "");
+	setcookie('login', null, mktime(0,0,0,01,01,1900), "/");
 	session_destroy();
 	header('Location: ../index.php');
 ?>
