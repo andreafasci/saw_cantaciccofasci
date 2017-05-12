805a5ed9f6195ffe095a855d052df5fb36c39c19
LcicC <cicco.4@live.com>
2017-05-12 17:59:54 +0200

    Add My_Profile Forms

---
+++ my_profile.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<head>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--CHANGE EMAIL FORM-->
+  <form>
+    <div class="form-group row">
+      <label class="col-sm-2 col-form-label">Your Email</label>
+      <div class="col-sm-10">
+        <p class="form-control-static">email@example.com</p>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="new_mail" class="col-sm-2 col-form-label">Type New Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="new_mail">
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Confirm</button>
+  </form>
+  <!--//////////////////////////////////////////////////////////////-->
+
+  <!--CHANGE NAME FORM-->
+  <form>
+    <div class="form-group row">
+      <label class="col-sm-2 col-form-label">Your Name</label>
+      <div class="col-sm-10">
+        <p class="form-control-static">email@example.com</p>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="new_name" class="col-sm-2 col-form-label">Type New Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="new_name">
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Confirm</button>
+  </form>
+  <!--//////////////////////////////////////////////////////////////-->
+
+  <!--CHANGE EMAIL FORM-->
+   <form>
+   <div class="form-group row">
+      <label for="pwd_1" class="col-sm-2 col-form-label">Type New Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="pwd_1">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="pwd_2" class="col-sm-2 col-form-label">Retype Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="pwd_2">
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Confirm</button>
+  </form> 
+  <!--//////////////////////////////////////////////////////////////-->
+</body>
